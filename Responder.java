dff459d0ac1966f4e42cac1df0199ea5063b4d43
David DiBenedetto <david@njweb.solutions>
2024-12-17 15:52:09 -0500

    Added Javadoc comments for the Responder class

--- Responder.java
+++ Responder.java
@@ -17,6 +17,17 @@ import java.util.*;
  * @author David J. Barnes and Michael Kölling.
  * @version 2016.02.29
  */
+/**
+ * The Responder class handles responses for a support system.
+ * 
+ * This class allows the loading of default responses and dynamic responses
+ * from a file. It also supports multi-line responses separated by blank lines.
+ * 
+ * Responsibilities:
+ * 1. Populate responses using a default list.
+ * 2. Populate responses dynamically using an input file.
+ * 3. Provide methods to access and print responses.
+ */
 public class Responder
 {
     // Used to map key words to responses.
@@ -35,35 +46,9 @@ public class Responder
         responseMap = new HashMap<>();
         defaultResponses = new ArrayList<>();
         fillResponseMap();
-        try (BufferedReader reader = new BufferedReader(new FileReader("responses.txt"))) {
-            String line;
-            String[] keys = null;
-            StringBuilder response = new StringBuilder();
-
-            while ((line = reader.readLine()) != null) {
-                if (line.trim().isEmpty()) {
-                    if (keys != null && response.length() > 0) {
-                        for (String key : keys) {
-                            responseMap.put(key.trim(), response.toString().trim());
-                        }
-                        response.setLength(0);
-                        keys = null;
-                    }
-                } else if (keys == null) {
-                    keys = line.split(",");
-                } else {
-                    response.append(line).append(" ");
-                }
-            }
-
-            if (keys != null && response.length() > 0) {
-                for (String key : keys) {
-                    responseMap.put(key.trim(), response.toString().trim());
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
+        fillDefaultResponses();
+        randomGenerator = new Random();
+    }
 
     /**
      * Generate a response from a given set of input words.
@@ -92,35 +77,54 @@ public class Responder
      * into our response map.
      */
     private void fillResponseMap()
-        try (BufferedReader reader = new BufferedReader(new FileReader("responses.txt"))) {
-            String line;
-            String[] keys = null;
-            StringBuilder response = new StringBuilder();
-
-            while ((line = reader.readLine()) != null) {
-                if (line.trim().isEmpty()) {
-                    if (keys != null && response.length() > 0) {
-                        for (String key : keys) {
-                            responseMap.put(key.trim(), response.toString().trim());
-                        }
-                        response.setLength(0);
-                        keys = null;
-                    }
-                } else if (keys == null) {
-                    keys = line.split(",");
-                } else {
-                    response.append(line).append(" ");
-                }
-            }
-
-            if (keys != null && response.length() > 0) {
-                for (String key : keys) {
-                    responseMap.put(key.trim(), response.toString().trim());
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
+    {
+        responseMap.put("crash", 
+                        "Well, it never crashes on our system. It must have something\n" +
+                        "to do with your system. Tell me more about your configuration.");
+        responseMap.put("crashes", 
+                        "Well, it never crashes on our system. It must have something\n" +
+                        "to do with your system. Tell me more about your configuration.");
+        responseMap.put("slow", 
+                        "I think this has to do with your hardware. Upgrading your processor\n" +
+                        "should solve all performance problems. Have you got a problem with\n" +
+                        "our software?");
+        responseMap.put("performance", 
+                        "Performance was quite adequate in all our tests. Are you running\n" +
+                        "any other processes in the background?");
+        responseMap.put("bug", 
+                        "Well, you know, all software has some bugs. But our software engineers\n" +
+                        "are working very hard to fix them. Can you describe the problem a bit\n" +
+                        "further?");
+        responseMap.put("buggy", 
+                        "Well, you know, all software has some bugs. But our software engineers\n" +
+                        "are working very hard to fix them. Can you describe the problem a bit\n" +
+                        "further?");
+        responseMap.put("windows", 
+                        "This is a known bug to do with the Windows operating system. Please\n" +
+                        "report it to Microsoft. There is nothing we can do about this.");
+        responseMap.put("macintosh", 
+                        "This is a known bug to do with the Mac operating system. Please\n" +
+                        "report it to Apple. There is nothing we can do about this.");
+        responseMap.put("expensive", 
+                        "The cost of our product is quite competitive. Have you looked around\n" +
+                        "and really compared our features?");
+        responseMap.put("installation", 
+                        "The installation is really quite straight forward. We have tons of\n" +
+                        "wizards that do all the work for you. Have you read the installation\n" +
+                        "instructions?");
+        responseMap.put("memory", 
+                        "If you read the system requirements carefully, you will see that the\n" +
+                        "specified memory requirements are 1.5 giga byte. You really should\n" +
+                        "upgrade your memory. Anything else you want to know?");
+        responseMap.put("linux", 
+                        "We take Linux support very seriously. But there are some problems.\n" +
+                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
+                        "more precise?");
+        responseMap.put("bluej", 
+                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
+                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
+                        "do about it, I'm afraid.");
+    }
 
     /**
      * Build up a list of default responses from which we can pick
